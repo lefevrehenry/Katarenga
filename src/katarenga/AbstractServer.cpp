90d1d1a85c0279aa9b236c798aca58f63c7f8430
Mommessc <clement.mommessin@inria.fr>
2020-02-17 17:07:50 +0100

    Bye bye server_communication and network_utils

--- src/katarenga/AbstractServer.cpp
+++
@@ -1,1 +0,0 @@
-#include "AbstractServer.hpp"
--- src/katarenga/server_communication.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef SERVER_COMMUNICATION_HPP
-#define SERVER_COMMUNICATION_HPP
-
-void server_communication_function();
-
-#endif  // SERVER_COMMUNICATION_HPP
